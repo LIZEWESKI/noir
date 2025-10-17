8844edefaf2eb0064d885d28b80a4065dc759f6c
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-10-18 00:24:30 +0100

    Rebuild with eager imports

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,14 +5,21 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 import { initializeTheme } from './hooks/use-appearance';
 const appName = import.meta.env.VITE_APP_NAME || 'Noir';
+const pages = import.meta.glob('./pages/**/*.jsx', { eager: true });
 
 createInertiaApp({
     title: (title) => `${title ? title : appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./pages/${name}.jsx`,
-            import.meta.glob('./pages/**/*.jsx'),
-        ),
+    resolve: (name) => {
+        
+        const pagePath = `./pages/${name}.jsx`;
+        const page = pages[pagePath];
+
+        if (!page) {
+            throw new Error(`Page not found: ${pagePath}`);
+        }
+
+        return page.default;
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 
