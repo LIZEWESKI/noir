ca75d8be5b0f03146d405e7d8c000a2b31735947
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-10-18 21:07:22 +0100

    sign up page is also supporting quick login now

--- resources/js/pages/auth/register.jsx
+++ resources/js/pages/auth/register.jsx
@@ -3,14 +3,26 @@ import { Head } from '@inertiajs/react';
 import Layout from '@/layouts/layout';
 import { RegisterForm } from '@/components/register-form';
 import { DemoAccounts } from '@/components/demo/demo-accounts';
-
+import { useMediaQuery } from 'react-responsive';
+import { Separator } from '@/components/ui/separator';
 const Register = ({demo_accounts}) => {
+  const isMobile = useMediaQuery({query:'(max-width: 1034px)'});
   return (
     <>
         <Head title="Sign up"/>
         <div className="flex min-h-svh flex-col items-center justify-center gap-8 p-6 md:p-10 relative">
           <RegisterForm />
-          <DemoAccounts demoAccounts={demo_accounts}/>
+          <DemoAccounts 
+            demoAccounts={demo_accounts} 
+            isMobile={isMobile} 
+            isAbsolute
+          />
+          {isMobile && 
+            <>
+              <Separator />
+              <DemoAccounts demoAccounts={demo_accounts} isMobile={!isMobile} />
+            </>
+          }
         </div>
     </>
   )
