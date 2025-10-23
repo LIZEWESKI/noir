12aa8fbdb1a37ed4233c35542fe6b8033c53e58a
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-10-23 19:52:04 +0100

    updated meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,8 +11,14 @@
         <meta property="og:description" content="Your Stay, Your Way. Noir's Personalized Hospitality.">
         <meta property="og:type" content="website">
         <meta property="og:url" content="https://noir.lizeweski.me/">
-        <meta property="og:image" content="{{ asset('noir-thumbnail.png') }}">
+        <meta property="og:image" content="{{ asset('noir-thumbnail.webp') }}">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630">
 
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Noir">
+        <meta name="twitter:description" content="Your Stay, Your Way. Noir's Personalized Hospitality.">
+        <meta name="twitter:image" content="{{ asset('noir-thumbnail.webp') }}">
         <!-- Fonts & Icon -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
