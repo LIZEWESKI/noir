b1e20b388969b60aaeb23e0b6c58324de1f864bd
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-10-23 19:32:16 +0100

    added new metas

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <meta name="description" content="Your Stay, Your Way. Noir's Personalized Hospitality.">
+        <meta name="author" content="Lizeweski">
+
+        <meta property="og:title" content="Noir">
+        <meta property="og:description" content="Your Stay, Your Way. Noir's Personalized Hospitality.">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://noir.lizeweski.me/">
+        <meta property="og:image" content="{{ asset('noir-thumbnail.png') }}">
+
         <!-- Fonts & Icon -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
